5c30c5bdbe5f8e3898cb7facc75ceb4fe29a1df4
Cody <cody.tracy92@gmail.com>
2019-06-03 13:12:44 -0500

    set up the server, two very basic pages, and the app page to test the Router

---
+++ google-books/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
+function App() {
+    return(
+        <Router>
+            <div>
+                <Route exact path="/" component={Search} />;
+                <Route exact path="/search" component={Search} />;
+                <Route exact path="/saved" component={Saved} />;
+            </div>
+        </Router>
+    )
+}
+
+export default App;
---
+++ google-books/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Search(){
+    return (
+        <div>
+            <p>
+                This Search page is working. 
+            </p>
+        </div>
+    )
+}
+
+export default Search;
